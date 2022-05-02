25b2eba98fa026be373e61f5dc2a6a8ab513a59e
carolcortes <caroline.ocortes@gmail.com>
2022-05-01 21:54:56 -0300

    exercicio 6.1

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-reponsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-reponsivo/dia-1-html-css-forms/script.js
@@ -1,1 +1,20 @@
 console.log('Olá, mundo!')
+
+const submitBtn = document.getElementById('submit');
+const clearBtn = document.getElementById('clear');
+
+function handleSubmit(event) {
+  event.preventDefault();
+}
+submitBtn.addEventListener('click', handleSubmit);
+
+function clear() {
+  const input = document.querySelectorAll('input');
+  const textArea = document.querySelector('textarea');
+  for (let index = 0; index < input.length; index += 1) {
+    input[index].value = '';
+    input[index].checked = false;
+  }
+  textArea = '';
+}
+clearBtn.addEventListener('click', clear);
